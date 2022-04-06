8aa61d6be6944f921c16e761dd08c559d461b00a
Lucas Dias Nardino <lucas.diasnardino@gmail.com>
2022-04-06 14:34:24 -0300

    Using useState

--- src/index.js
+++ src/index.js
@@ -1,35 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
 
-function Lake({name}){
+function App(){
+  const [status, setStatus] = useState("Open");
   return(
     <div>
-      <h1>Visit {name}!</h1>
-    </div>
+      <h1>Status: {status}</h1>
+      <button onClick={() => setStatus("Open")}>
+        Open
+      </button>
+      <button onClick={() => setStatus("Back in 5")}>
+        Break
+      </button>
+      <button onClick={() => setStatus("Closed")}>
+        Closed
+      </button>
+      </div>
   );
 }
-
-function SkiResort({name}){
-  return(
-    <div>
-      <h1>Visit {name}!</h1>
-    </div>
-  );
-}
-
-
-function App(props){
-  return(
-    <>
-      <Lake />
-      <SkiResort/>
-    </>
-  )
-}
-
 
 ReactDOM.render(
   <App />,
