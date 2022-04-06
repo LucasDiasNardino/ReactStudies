eccef1848930594725e51e0f8edbce2c0ed0bd9e
Lucas Dias Nardino <lucas.diasnardino@gmail.com>
2022-04-06 14:43:21 -0300

    multiple State Variables

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function App(){
+  const [year, setYear ] = useState(2050);
+  const [manager, setManager] = useState("Alex");
   const [status, setStatus] = useState("Open");
   return(
+    <>
+    <div>
+      <h1>{year}</h1>
+      <button onClick={() => setYear(year + 1)}>
+        Happy New Year!
+      </button>
+    </div>
+    <div>
+      <h1>Manager on Duty: {manager}</h1>
+      <button onClick={() => setManager("Rachel")}>New Manager</button>
+    </div>
     <div>
       <h1>Status: {status}</h1>
       <button onClick={() => setStatus("Open")}>
@@ -16,7 +29,8 @@ function App(){
       <button onClick={() => setStatus("Closed")}>
         Closed
       </button>
-      </div>
+    </div>
+    </>
   );
 }
 
