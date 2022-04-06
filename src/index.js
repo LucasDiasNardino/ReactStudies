bafb951c925f55a3d73c2409ab432e11574380f6
Lucas Dias Nardino <lucas.diasnardino@gmail.com>
2022-04-06 10:56:37 -0300

    Components

--- src/index.js
+++ src/index.js
@@ -4,28 +4,35 @@ import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
-function Hello(){
+function Hello(props){
+  console.log(props);
   return(
     <div>
-      <h1>Welome to React!</h1>
-      <p>Let's build something cool.</p>
+      <h1>Welome to {props.library}!</h1>
+      <p>{props.message}</p>
+      <p>{props.number} Props  In Total</p>
+      {/* <p>
+        {Object.keys(props).length} Props Total
+      </p> */}
     </div>
   )
 }
 
+// function Hello({library, message, number}){
+//   return(
+//     <div>
+//       <h1>Welome to {library}!</h1>
+//       <p>{message}</p>
+//       <p>{number} Props Total</p>
+//     </div>
+//   )
+// }
+
 ReactDOM.render(
-  <Hello/>,
+  <Hello 
+  library="react" 
+  message="Have Fun!" 
+  number = {3}
+  />,
   document.getElementById("root")
 );
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
