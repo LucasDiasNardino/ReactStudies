cab242a6f6aedf8a46b906752b9f83649cc2079a
Lucas Dias Nardino <lucas.diasnardino@gmail.com>
2022-04-06 15:13:49 -0300

    useEffect Dependency Array

--- src/index.js
+++ src/index.js
@@ -2,25 +2,40 @@ import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function CheckBox(){
-  const [checked, setChecked] = useState(false);
+function App(){
+  const [val, setVal] = useState("");
+  const [val2, setVal2] = useState("");
+
+  useEffect(() => {
+    console.log(`field 1: ${val}`);
+  }, [val]);
+
   useEffect(() =>{
-    alert(`checked: ${checked.toString()}`);
-  });
+    console.log(`field2: ${val2}`);
+  }, [val, val2]);
+
   return(
     <>
-      <input 
-        type="checkbox" 
-        value={checked}
-        onChange={() => 
-          setChecked(checked => !checked)}
-      />
-      {checked ? "checked" : "not checked"}
+      <label>
+        Favourite Phrase:
+        <input 
+          value={val} 
+          onChange={e => setVal(e.target.value)}
+        />
+      </label>
+      <br/>
+      <label>
+        Second Favorite Phrase:
+        <input 
+          value={val2} 
+          onChange={e => setVal2(e.target.value)}
+        />
+      </label>
     </>
   )
 }
 
 ReactDOM.render(
-  <CheckBox />,
+  <App />,
   document.getElementById("root")
 );
