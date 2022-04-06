42fffe69a46a9b7d3234cda20d068b8520296f3b
Lucas Dias Nardino <lucas.diasnardino@gmail.com>
2022-04-06 11:19:01 -0300

    Rendering lists of objects

--- src/index.js
+++ src/index.js
@@ -5,31 +5,26 @@ import './index.css';
 // import reportWebVitals from './reportWebVitals';
 
 const lakeList = [
-  "Echo Lake",
-  "Maud Lake",
-  "Cascade Lake",
+  {id: "1", name:"Echo", trailhead: "Echo"},
+  {id: "2", name:"Maud", trailhead: "Wrights"},
+  {id: "3", name:"Velma", trailhead: "Bayview"},
 ];
 
 
-function App(props){
+function App({lakes}){
   return (
-    <ul>
-      {props.lakes.map(lake => (
-      <li>{lake}</li>
+    <div>
+      {lakes.map( lake => (
+        <div>
+          <h2>{lake.name}</h2>
+          <p>Accessed by: {lake.trailhead}</p>
+        </div>
       ))}
-    </ul>
+    
+    </div>
   );
 }
 
-// function App({lakes}){
-//   return (
-//     <ul>
-//       {lakes.map(lake => (
-//       <li>{lake}</li>
-//       ))}
-//     </ul>
-//   );
-// }
 
 ReactDOM.render(
   <App lakes={lakeList}/>,
