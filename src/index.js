4a592012e44588d14b6f7619f96330b974aaefe5
Lucas Dias Nardino <lucas.diasnardino@gmail.com>
2022-04-06 15:33:16 -0300

    Fetching Data

--- src/index.js
+++ src/index.js
@@ -2,37 +2,30 @@ import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function App(){
-  const [val, setVal] = useState("");
-  const [val2, setVal2] = useState("");
+function GitHubUser({login}){
+  const [data, setData] = useState(null);
 
   useEffect(() => {
-    console.log(`field 1: ${val}`);
-  }, [val]);
+    fetch(`https://api.github.com/user/${login}`)
+      .then(res => res.json())
+      .then(setData)
+      .catch(console.error);
+  }, [])
 
-  useEffect(() =>{
-    console.log(`field2: ${val2}`);
-  }, [val, val2]);
+  if(data){
+    return (
+      <div>
+        <h1>{data.login}</h1>
+        <img src={data.avatar_url} width={100}/>
+      </div>
+      )
+  }
+  return null;
+}
 
-  return(
-    <>
-      <label>
-        Favourite Phrase:
-        <input 
-          value={val} 
-          onChange={e => setVal(e.target.value)}
-        />
-      </label>
-      <br/>
-      <label>
-        Second Favorite Phrase:
-        <input 
-          value={val2} 
-          onChange={e => setVal2(e.target.value)}
-        />
-      </label>
-    </>
-  )
+
+function App(){
+  return <GitHubUser login="LucasDiasNardino"/>
 }
 
 ReactDOM.render(
