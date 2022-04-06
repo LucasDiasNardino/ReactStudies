71ea8f52627ee84b8549208865ef84fe2e5962f4
Lucas Dias Nardino <lucas.diasnardino@gmail.com>
2022-04-06 11:08:42 -0300

    Composing Components

--- src/index.js
+++ src/index.js
@@ -4,35 +4,21 @@ import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
-function Hello(props){
-  console.log(props);
-  return(
+function Lake({name}){
+  return <h1>{name}</h1>;
+}
+
+function App(){
+  return (
     <div>
-      <h1>Welome to {props.library}!</h1>
-      <p>{props.message}</p>
-      <p>{props.number} Props  In Total</p>
-      {/* <p>
-        {Object.keys(props).length} Props Total
-      </p> */}
+      <Lake name="Lake Tahoe" />
+      <Lake name="Angora Lake" />
+      <Lake name="Shirley Lake"/>
     </div>
   )
 }
 
-// function Hello({library, message, number}){
-//   return(
-//     <div>
-//       <h1>Welome to {library}!</h1>
-//       <p>{message}</p>
-//       <p>{number} Props Total</p>
-//     </div>
-//   )
-// }
-
 ReactDOM.render(
-  <Hello 
-  library="react" 
-  message="Have Fun!" 
-  number = {3}
-  />,
+  <App/>,
   document.getElementById("root")
 );
